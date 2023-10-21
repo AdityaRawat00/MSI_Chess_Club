82e7ee8d34d1783db9d82007ace8de400c63ea85
आदित्य रावत <scienceloveradityathegreat@gmail.com>
2023-10-21 18:22:26 +0530

    More users added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const url = "https://api.chess.com/pub/player/{Player}/stats";
+const datalink = "https://docs.google.com/spreadsheets/d/1t3dnHg8SWWsiSEbYSCvPATRly2nMUb4KXXHFKf0uZlU/edit#gid=379472038";
 
-const players = ["yourowncomrade", "vinayyd", "i_m_a_sniper", "ipranjalpant"];
+// const players = ["yourowncomrade", "vinayyd", "i_m_a_sniper", "ipranjalpant"];
+const players = ['I_M_A_Sniper', 'ipranjalpant', 'AkshatGarg1234', 'aaradhya0701', 'arjeetkitru', 'SanketD244','anmol_shuklaa', 
+                'Vkeves', 'ramkharbanda','YourOwnComrade', 'ujjwalchess3', 'Supriyo02', 'Manas_Bawari', 'Ishaan706', 'Albin77777', 
+                'dhruvv_bajaj', 'visionrajput','10Garv', 'namannn04', 'CHEEMSchessOP', 'DakshhJain',  'Ashutoshchezz',
+                'KRISH981155', 'Hashirama_shenju', 'Dev9076', 'dhruvrankoti', 'SIDDHARTH1357', 'Abhijitam', 'SARTHAK10710',
+                'mikhailtal345', 'alphaton', 'Arman202p', 'DAKUVANSH', 'Playerwr', 'DevTech03']
+// , , , , 'Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 'crushergaming']
 let player_data = [];
 
 async function getUser() {

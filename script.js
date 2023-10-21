c788bbe8a3d3bb4f52a432fe20342c73aaa784af
आदित्य रावत <scienceloveradityathegreat@gmail.com>
2023-10-21 20:17:35 +0530

    Synchronous data flow

--- script.js
+++ script.js
@@ -2,33 +2,35 @@ const url = "https://api.chess.com/pub/player/{Player}/stats";
 const datalink = "https://docs.google.com/spreadsheets/d/1t3dnHg8SWWsiSEbYSCvPATRly2nMUb4KXXHFKf0uZlU/edit#gid=379472038";
 
 // const players = ["yourowncomrade", "vinayyd", "i_m_a_sniper", "ipranjalpant"];
-const players = ['I_M_A_Sniper', 'ipranjalpant', 'AkshatGarg1234', 'aaradhya0701', 'arjeetkitru', 'SanketD244','anmol_shuklaa', 
-                'Vkeves', 'ramkharbanda','YourOwnComrade', 'ujjwalchess3', 'Supriyo02', 'Manas_Bawari', 'Ishaan706', 'Albin77777', 
-                'dhruvv_bajaj', 'visionrajput','10Garv', 'namannn04', 'CHEEMSchessOP', 'DakshhJain',  'Ashutoshchezz',
-                'KRISH981155', 'Hashirama_shenju', 'Dev9076', 'dhruvrankoti', 'SIDDHARTH1357', 'Abhijitam', 'SARTHAK10710',
-                'mikhailtal345', 'alphaton', 'Arman202p', 'DAKUVANSH', 'Playerwr', 'DevTech03']
+// const players = []
+
+//Error - Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at 
+//https://api.chess.com/pub/player/ujjwalchess3/stats. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing). 
+//Status code: 429
 // , , , , 'Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 'crushergaming']
+
+const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 
+'crushergaming','I_M_A_Sniper', 'ipranjalpant', 'AkshatGarg1234', 'aaradhya0701', 'arjeetkitru', 'SanketD244','anmol_shuklaa', 
+'Vkeves', 'ramkharbanda','YourOwnComrade', 'ujjwalchess3', 'Supriyo02', 'Manas_Bawari', 'Ishaan706', 'Albin77777', 
+'dhruvv_bajaj', 'visionrajput','10Garv', 'namannn04', 'CHEEMSchessOP', 'DakshhJain',  'Ashutoshchezz',
+'KRISH981155', 'Hashirama_shenju', 'Dev9076', 'dhruvrankoti', 'SIDDHARTH1357', 'Abhijitam', 'SARTHAK10710',
+'mikhailtal345', 'alphaton', 'Arman202p', 'DAKUVANSH'];
+
+// const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI'];
+
 let player_data = [];
 
 async function getUser() {
-    // Use map to create an array of promises
-    const promises = players.map(async function (d) {
-        const response = await fetch(`https://api.chess.com/pub/player/${d}/stats`);
+    for (const player of players) {
+        const response = await fetch(`https://api.chess.com/pub/player/${player}/stats`);
         const data = await response.json();
-        return data["chess_rapid"]["last"]["rating"];
-    });
-
-    // Wait for all promises to resolve
-    player_data = await Promise.all(promises);
-
-    console.log(player_data);
+        player_data.push(data["chess_rapid"]["last"]["rating"]);
+    }
 }
-
 drawGraph();
 
 async function drawGraph() {
     await getUser();
-    console.log("Second");
     const ctx = document.getElementById('chart');
 
     new Chart(ctx, {
@@ -51,4 +53,5 @@ async function drawGraph() {
     });
 }
 
-console.log("Script is working");
+
+

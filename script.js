a485551855c17d60dac4e3d4cca4bb5ea60cae92
आदित्य रावत <scienceloveradityathegreat@gmail.com>
2023-10-23 15:14:40 +0530

    Updated Js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const url = "https://api.chess.com/pub/player/{Player}/stats";
 const datalink = "https://docs.google.com/spreadsheets/d/1t3dnHg8SWWsiSEbYSCvPATRly2nMUb4KXXHFKf0uZlU/edit#gid=379472038";
 
-// const players = ["yourowncomrade", "vinayyd", "i_m_a_sniper", "ipranjalpant"];
-// const players = []
-
-//Error - Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at 
-//https://api.chess.com/pub/player/ujjwalchess3/stats. (Reason: CORS header ‘Access-Control-Allow-Origin’ missing). 
-//Status code: 429
-// , , , , 'Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 'crushergaming']
-
 const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 
 'crushergaming','I_M_A_Sniper', 'ipranjalpant', 'AkshatGarg1234', 'aaradhya0701', 'arjeetkitru', 'SanketD244','anmol_shuklaa', 
 'Vkeves', 'ramkharbanda','YourOwnComrade', 'ujjwalchess3', 'Supriyo02', 'Manas_Bawari', 'Ishaan706', 'Albin77777', 
@@ -16,21 +8,39 @@ const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshuk
 'KRISH981155', 'Hashirama_shenju', 'Dev9076', 'dhruvrankoti', 'SIDDHARTH1357', 'Abhijitam', 'SARTHAK10710',
 'mikhailtal345', 'alphaton', 'Arman202p', 'DAKUVANSH'];
 
-// const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI'];
-
 let player_data = [];
+
+let jsondata = [];
 
 async function getUser() {
     for (const player of players) {
         const response = await fetch(`https://api.chess.com/pub/player/${player}/stats`);
         const data = await response.json();
+        
+        // if(data["chess_rapid"]["last"]["rating"]){
+        // const rapid = data["chess_rapid"]["last"]["rating"];
+        // }
+
+        const rapid = data["chess_rapid"]?data["chess_rapid"]["last"]["rating"]:800;
+        
+        const blitz = data["chess_blitz"]?data["chess_blitz"]["last"]["rating"]:800;
+        const bullet = data["chess_bullet"]?data["chess_bullet"]["last"]["rating"]:800;
+        
+        const dic = {
+            [player]:[rapid, blitz, bullet]
+        };
+        jsondata.push(dic);
         player_data.push(data["chess_rapid"]["last"]["rating"]);
     }
 }
-drawGraph();
+// drawGraph();
 
 async function drawGraph() {
     await getUser();
+
+    let dic_string = JSON.stringify(jsondata);
+    var fs = require('fs');
+    fs.writeFile("chess_ratings.json", dic_string); 
     const ctx = document.getElementById('chart');
 
     new Chart(ctx, {
@@ -52,6 +62,3 @@ async function drawGraph() {
         }
     });
 }
-
-
-

0301d9ac27a9af74ebb91cb94b66815ce29887b7
आदित्य रावत <scienceloveradityathegreat@gmail.com>
2023-10-23 18:21:56 +0530

    New js added

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
 const url = "https://api.chess.com/pub/player/{Player}/stats";
 const datalink = "https://docs.google.com/spreadsheets/d/1t3dnHg8SWWsiSEbYSCvPATRly2nMUb4KXXHFKf0uZlU/edit#gid=379472038";
 
-const players = ['Playerwr', 'DevTech03', 'ishaanvats', 'pankaj1238','priyxnshukumar', 'VisheshSinghal1510', 'SIDDHARTHalsoNEGI', 
-'crushergaming','I_M_A_Sniper', 'ipranjalpant', 'AkshatGarg1234', 'aaradhya0701', 'arjeetkitru', 'SanketD244','anmol_shuklaa', 
-'Vkeves', 'ramkharbanda','YourOwnComrade', 'ujjwalchess3', 'Supriyo02', 'Manas_Bawari', 'Ishaan706', 'Albin77777', 
-'dhruvv_bajaj', 'visionrajput','10Garv', 'namannn04', 'CHEEMSchessOP', 'DakshhJain',  'Ashutoshchezz',
-'KRISH981155', 'Hashirama_shenju', 'Dev9076', 'dhruvrankoti', 'SIDDHARTH1357', 'Abhijitam', 'SARTHAK10710',
-'mikhailtal345', 'alphaton', 'Arman202p', 'DAKUVANSH'];
-
 let player_data = [];
+let user_name = [];
 
 let jsondata = [];
 
-async function getUser() {
-    for (const player of players) {
-        const response = await fetch(`https://api.chess.com/pub/player/${player}/stats`);
-        const data = await response.json();
-        
-        // if(data["chess_rapid"]["last"]["rating"]){
-        // const rapid = data["chess_rapid"]["last"]["rating"];
-        // }
+fetch("chess_data.csv")
+    .then(response=>response.text())
+    .then(csvdata => {
+        const row = csvdata.split("\n").map(row => row.split(","))
+        for(let r of row) 
+            jsondata.push(row);
+    });
+console.log(jsondata);
 
-        const rapid = data["chess_rapid"]?data["chess_rapid"]["last"]["rating"]:800;
-        
-        const blitz = data["chess_blitz"]?data["chess_blitz"]["last"]["rating"]:800;
-        const bullet = data["chess_bullet"]?data["chess_bullet"]["last"]["rating"]:800;
-        
-        const dic = {
-            [player]:[rapid, blitz, bullet]
-        };
-        jsondata.push(dic);
-        player_data.push(data["chess_rapid"]["last"]["rating"]);
-    }
-}
-// drawGraph();
+drawGraph();
 
-async function drawGraph() {
-    await getUser();
-
-    let dic_string = JSON.stringify(jsondata);
-    var fs = require('fs');
-    fs.writeFile("chess_ratings.json", dic_string); 
+function drawGraph() {
     const ctx = document.getElementById('chart');
-
-    new Chart(ctx, {
+       new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: players,
+            labels: ['a','b','c', 'd', 'e'],
             datasets: [{
                 label: 'Chess Rating',
-                data: player_data,
+                data: [1,3,5,6,7],
                 borderWidth: 1
             }]
         },

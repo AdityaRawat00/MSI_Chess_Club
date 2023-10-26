4c2980ac93cbbc3c165b174044e29645ae2e453b
आदित्य रावत <scienceloveradityathegreat@gmail.com>
2023-10-26 23:20:13 +0530

    Graph switch

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let user_name = [];
 let rapid = [];
 let blitz = [];
 let bullet = [];
-
+let data = []
 let avg_values = [];
 
 async function getData(){
-    const scriptPath = document.currentScript.src;
-    const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
-    const csvPath = basePath + "chessdata.txt";
+    // const scriptPath = document.currentScript.src;
+    // const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
+    const csvPath = "chessdata.txt";
     const response = await fetch(csvPath);
     const data = await response.text();
 
@@ -30,34 +30,39 @@ async function getData(){
     avg_values[3] = rows[rows.length-1].split(',')[4];
 }
 
-displayGraphTable();
 
-async function displayGraphTable(){
+async function displayGraphTable(current = 0){
     //Rapid
+    if(current == 0)
     drawGraphRapid(rapid, "tableRapid", "chartRapid");   
-    //Blitz
-    drawGraphRapid(blitz, "tableBlitz", "chartBlitz"); 
-    //Bullet
-    drawGraphRapid(bullet, "tableBullet", "chartBullet"); 
+//Blitz
+else if(current == 1)
+drawGraphRapid(blitz, "tableBlitz", "chartBlitz"); 
+
+//Bullet
+else
+drawGraphRapid(bullet, "tableBullet", "chartBullet"); 
 }
 
 
 async function drawGraphRapid(typeofgraph, tableId, chartId) {
-    await getData();
-
-    const data = user_name.map((username, index) => ({ username, rating: typeofgraph[index]}));
-
+    
+    if (data.length == 0)
+        await getData();
+    
+    data = user_name.map((username, index) => ({ username, rating: typeofgraph[index]}));
+    
     // Sorting Data in ascending and desceding
     const ascendingData = [...data].sort((a, b) => a.rating - b.rating);
     const descendingData = [...data].sort((a, b) => b.rating - a.rating);
-
+    
     const ascUserName = ascendingData.map(item=>item.username);
     const ascRating = ascendingData.map(item => item.rating);
-
+    
     const desUserName = descendingData.map(item=>item.username);
     const descRating = descendingData.map(item=>item.rating);
     //end
-
+    
     const tableBody = document.querySelector(`#${tableId} tbody`);
     let x = 0;
     let avgIndex = 20;
@@ -72,14 +77,14 @@ async function drawGraphRapid(typeofgraph, tableId, chartId) {
             console.log(i);
         }
     }
-
+    
     document.getElementById("rapid").textContent = avg_values[0];
     document.getElementById("blitz").textContent = avg_values[1];
     document.getElementById("bullet").textContent = avg_values[2];
     document.getElementById("puzzle").textContent = avg_values[3];
     console.log(avg_values[3]);
     document.getElementById("total").textContent = rapid.length;
-
+    
     const ctx = document.getElementById(`${chartId}`);
     new Chart(ctx, {
         type: 'bar',
@@ -96,34 +101,24 @@ async function drawGraphRapid(typeofgraph, tableId, chartId) {
             },
         ],
     },
-        options: {
-            scales: {
-                x: {
-                    ticks: {
-                        autoSkip: false
-                    }
-                },
-            }
+    options: {
+        scales: {
+            x: {
+                ticks: {
+                    autoSkip: false
+                }
+            },
         }
-    });
+    }
+});
 }
 
+//calling for default graph
+openGraph('chartRapid','tableRapid', 0);
 
-function openTab(gameType)
+async function openGraph(graphType,tableType, cur)
 {
-    
-  var i;
-  var x = document.getElementsByClassName("optionContent");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  document.getElementById(gameType).style.display = "block";  
-}
-
-
-
-function openGraph(graphType,tableType)
-{
+    await displayGraphTable(cur);
     var i;
     var x = document.getElementsByClassName("ratingGraph");
     for (i = 0; i < x.length; i++) {
@@ -136,4 +131,16 @@ function openGraph(graphType,tableType)
     }
     document.getElementById(graphType).style.display = "block";
     document.getElementById(tableType).style.display = "block";
+}
+
+//Not being used
+function openTab(gameType)
+{
+    
+    var i;
+    var x = document.getElementsByClassName("optionContent");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    document.getElementById(gameType).style.display = "block";  
 }
